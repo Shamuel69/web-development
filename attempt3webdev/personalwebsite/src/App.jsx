c24d7efc077ce404fee0fbc8bc64fe309047f0b2
Samuel the Ultimate <tobbanganmantis@gmail.com>
2025-10-07 03:16:04 -0400

    working on adding style to certain elements before big changes

--- attempt3webdev/personalwebsite/src/App.jsx
+++ attempt3webdev/personalwebsite/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   const [darkMode, setDarkMode] = useState(false);
   const iconSrc = darkMode ? LightMode : DarkMode;
   const [bgColor, setBgColor] = useState(darkMode ? '#333' : '#fff');
-  const shineColor = bgColor ? '#fff' : '#000';
+  
   document.body.style.backgroundColor = bgColor;
 
   React.useEffect(() => {
@@ -27,7 +27,8 @@ function App() {
   return (
     <>
       <div>
-        <img src={iconSrc} className="logo" alt="light/dark toggle" onClick={() => setDarkMode(!darkMode)}/>
+        <img src={iconSrc} className="logo" alt="light/dark toggle" 
+          onClick={() => setDarkMode(!darkMode)} style={{cursor: 'pointer', filter: darkMode ? 'dropshadow(0 0 2em #646cffaa)' : 'dropshadow(0 0 2em #ecfb61aa)'}}/>
       </div>
       <div className="big_banner">
       <h1>Welcome to my personal website!</h1>
