f0a3f3993b2d583983a4c9abf64804e8521700f1
Samuel the Ultimate <tobbanganmantis@gmail.com>
2025-10-23 20:30:46 -0400

    so many bugs kms

--- attempt3webdev/personalwebsite/src/App.jsx
+++ attempt3webdev/personalwebsite/src/App.jsx
@@ -18,34 +18,43 @@ function App() {
   const [darkMode, setDarkMode] = useState(false);
   const iconSrc = darkMode ? LightMode : DarkMode;
   
+  const [IsOpen, setIsOpen] = useState(false);
+
   React.useEffect(() => {
     document.body.style.backgroundColor = darkMode ? '#333' : '#271a26ff';
   }, [darkMode]);
   return (
     <div className="app">
-      <header className={"header ${darkMode ? 'dark' : 'light'}"}>
-        <h1>Personal Website</h1>
-        <img src={iconSrc} className="logo" alt="light/dark toggle" 
-          onClick={() => setDarkMode(!darkMode)} style={{cursor: 'pointer'}}/>
-      </header>
-
-      <div className="big_banner">
-        <h1>Welcome to my personal website!</h1>
+      <div className="text-header-container" style={{position: "absolute", left: 0}}>
+        <header className="header" style={{}}>
+          <h1>Personal Website</h1>
+          <img src={iconSrc} className="logo" alt="light/dark toggle" 
+            onClick={() => setDarkMode(!darkMode)} style={{cursor: 'pointer'}}/>
+        </header>
       </div>
-      
-      <h2>The date if you missed it: {formatDate(new Date())}</h2>
-      
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div className="sidebar">
+        <button onClick={() => setIsOpen(!IsOpen)}> close/open </button>
+        <h2>Sidebar</h2>
+      </div>
+      <div className="inside-content">
+        <div className="big_banner">
+          <h1>Welcome to my personal website!</h1>
+        </div>
+        
+        <h2>The date if you missed it: {formatDate(new Date())}</h2>
+        
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
