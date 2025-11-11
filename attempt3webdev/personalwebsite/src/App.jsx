6adadfcd66406a23a82325127e2335a528f1b37f
Samuel the Ultimate <tobbanganmantis@gmail.com>
2025-11-11 02:12:43 -0500

    made the sidebar more dynamic, just needs tinkering in layout

--- attempt3webdev/personalwebsite/src/App.jsx
+++ attempt3webdev/personalwebsite/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import LightMode from './assets/sun_with_clouds.png'
 import DarkMode from './assets/crescent_moon.png'
+import closedBook from './assets/closed_book.svg'
+import openedBook from './assets/opened_book.svg'
 import './App.css'
 
 function formatDate(date) {
@@ -16,34 +18,42 @@ function formatDate(date) {
 function App() {
   const [count, setCount] = useState(0)
   const [darkMode, setDarkMode] = useState(false);
-  
+
   const iconSrc = darkMode ? LightMode : DarkMode;
   
   const [IsOpen, setIsOpen] = useState(false);
-
+  const bookiconSrc = IsOpen ? openedBook : closedBook;
+  
   React.useEffect(() => {
     document.body.style.backgroundColor = darkMode ? '#333' : '#271a26ff';
   }, [darkMode]);
   return (
     <div className="app">
       <div className="text-header-container" style={{borderBottom: '2px solid white'}}>
-          <h1>Personal Website</h1>
-          <img src={iconSrc} className="logo" alt="light/dark toggle" 
-            onClick={() => setDarkMode(!darkMode)} style={{cursor: 'pointer'}}/>
-      </div>
-      <div className="sidebar">
-        <p>status: {IsOpen ? 'open' : 'closed'}</p>
-        <button onClick={() => setIsOpen(!IsOpen)}> close/open </button>
-        <h2>Sidebar</h2>
-        <div className="directory-links" style={{display: IsOpen ? 'block' : 'none'}}>
-          <p>bling bling</p>
-          <ul className="links-list">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About Me</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
+        <h1>Personal Website</h1>
+        <img src={iconSrc} className="logo" alt="light/dark toggle" 
+          onClick={() => setDarkMode(!darkMode)} style={{cursor: 'pointer'}}/>
+      <div className={`sidebar ${IsOpen ? "active" : ""}`}>
+        <div className="sidebar-icon-container">
+          <img src={bookiconSrc} className="book-icon" alt="toggle open and close sidebar"
+            onClick={() => setIsOpen(!IsOpen)} style={{cursor: 'pointer'}}/>
         </div>
+        <div className={`ease-in-out ${IsOpen ? "active" : ""}`}>
+          <p>status: {IsOpen ? 'active' : 'closed'}</p>
+          <h2>Sidebar</h2>
+          <h1>ghghg</h1>
+          <div className="directory-links" >
+            <p>bling bling</p>
+            <ul className="links-list">
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About Me</a></li>
+              <li><a href="#projects">Projects</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div>
+        </div>
+        
+      </div>
       </div>
       <div className="inside-content" style={{width: '60%'}}>
         <div className="big_banner">
