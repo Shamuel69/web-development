e0da6e87f446084cb63c3fee23546e15980c6b8f
Samuel the Ultimate <tobbanganmantis@gmail.com>
2025-12-28 23:36:19 -0500

    tinkering with more optimized ways to make headers

--- attempt3webdev/shoppingapp/shopping_app/src/App.jsx
+++ attempt3webdev/shoppingapp/shopping_app/src/App.jsx
@@ -1,42 +1,45 @@
 import { useState } from 'react'
 import profile from './assets/profile-icon.svg'
 import Search from './assets/search-icon.svg'
+import shoppingCart from './assets/shopping-cart.svg'
 import { Routes, Route, Link } from 'react-router-dom';
 import './App.css'
 
 function App() {
   const [isshown, setIsShown] = useState(false);
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div className="App">
         <div className="App-header">
-          <h2>Shopping App</h2>
-          <div className="App-profile">
-            <img src={profile} alt="Profile" />
-            <h4>Sign In</h4>
+          <div className="App-title-container">
+            <h2>Shopping App</h2>
+            <div className="search-bar" style={{cursor: 'pointer'}}>
+              <img src={Search} alt="Search" onClick={() => setIsShown(!isshown)} />
+              <input className={`search-bar-header ${isshown ? 'show' : ''}`} type="text" placeholder="Search..."/>
+            </div>
           </div>
-          <div className="search-bar">
-            <img src={Search} alt="Search" onClick={() => setIsShown(!isshown)} />
-            <input hidden={!isshown} type="text" placeholder="Search..."/>
+          {/* <div className="App-title-container">
+            <Link to="/"><h2>Home</h2></Link>
+            <Link to="/about"><h2>About</h2></Link>
+            <Link to="/contact"><h2>Contact</h2></Link>
+          </div> */}
+          <div className="App-title-container">
+            <div className="App-profile">
+              <img src={profile} alt="Profile" />
+              <h4>Sign In</h4>
+            </div>
+            <img className="App-cart" src={shoppingCart} alt="Cart" />
           </div>
         </div>
-        <div className="App-menu">to be added: contacts, cart, checkout, deals, recent adds</div>
+        <div className="App-menu">
+        
+          <Link to="/">Jewels</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        
+        </div>
       </div>
-
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
