46db45cb5b7730d098a87798e84be65d3663a6f9
Samuel the Ultimate <tobbanganmantis@gmail.com>
2026-04-07 22:43:52 -0400

    bingus

--- attempt3webdev/shoppingapp/shopping_app/src/subpages/InventoryItem.jsx
+++ attempt3webdev/shoppingapp/shopping_app/src/subpages/InventoryItem.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect} from "react";
-import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 import './css/inventory.css'
 import './css/inventoryitem.css'
@@ -9,10 +9,42 @@ import necklace from '../assets/necklacemodel.jpg'
 import earrings from '../assets/earring.jpg'
 import bracelets from '../assets/bracelet.jpg'
 
-function InventoryItem({ item }) {
+
+function InventoryItem() {
+    const {id} = useParams();
+    const [item, setItem] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const images = {
+        "Ring": ring,
+        "Necklace": necklace,
+        "Earrings": earrings,
+        "Bracelets": bracelets
+    };
+
+    useEffect(() => {
+        const fetchedItem = async () => {
+            try {
+                const res = await fetch(`http://localhost:8080/inventory/${id}`);
+                // if (!res.ok) throw new Error("Item not found" + id);
+                const data = await res.json();
+                setItem(data.item);
+            } catch (error) {
+                console.error("Error fetching item:", error);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        fetchedItem();
+    }, [id]);
+
+    if (!item) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div className="item-card">
-            <img src={item.image} alt="Random Ring"/>
+            <img src={images[item.label]} alt="Random Ring"/>
             <div className="item-info">
                 <h3>{item.name}</h3>
                 <p>Original Price: <i>${item.originalPrice}</i></p>
